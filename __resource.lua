82d7427b151ec04c63f0dee565afb8d0929adf85
brefaby <40739200+brefaby@users.noreply.github.com>
2019-07-12 16:31:55 +0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+description 'LOADING SCREEN | Made by XTREME and BREFABY'
+
+files {
+    "/loadscreenpublic/copycat.ogg",
+    "/loadscreenpublic/css/base.css",
+    "/loadscreenpublic/css/demo3.css",
+    "/loadscreenpublic/img/1.jpg",
+    "/loadscreenpublic/img/2.jpg",
+    "/loadscreenpublic/img/3.jpg",
+    "/loadscreenpublic/img/4.jpg",
+    "/loadscreenpublic/img/5.jpg",
+    "/loadscreenpublic/img/6.jpg",
+    "/loadscreenpublic/index3.html",
+    "/loadscreenpublic/js/demo.js",
+    "/loadscreenpublic/js/imagesloaded.pkgd.min.js"
+}
+
+loadscreen 'index3.html'
